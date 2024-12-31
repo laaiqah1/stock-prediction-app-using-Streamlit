9f3a019cfe50f61180e6252e5c9c2b008b971082
laaiqah1 <mat23la.shaikh@pg.ictmumbai.edu.in>
2024-12-31 08:54:19 +0530

    Create rnn_model.py

---
+++ models/rnn_model.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import SimpleRNN, Dense, Dropout
+
+# Build RNN model
+def build_rnn_model(input_shape):
+    model = Sequential()
+    model.add(SimpleRNN(units=50, return_sequences=True, input_shape=input_shape))
+    model.add(Dropout(0.2))
+    model.add(SimpleRNN(units=50, return_sequences=False))
+    model.add(Dropout(0.2))
+    model.add(Dense(units=1))  # Output the predicted stock price
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    return model
